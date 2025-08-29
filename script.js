ee04ca9749f9df7e0d78930d5e8fb7a60462d3eb
chaiiiiy <clepilepsia@gmail.com>
2025-08-29 16:08:04 +0800

    Link buttons to game logic, output text to dialogue box

--- script.js
+++ script.js
@@ -6,26 +6,30 @@
 
 const startScreen = document.querySelector("#start-screen");
 const gameScreen = document.querySelector("#game-screen");
-const winScreen = document.querySelector("#win-screen");
-const loseScreen = document.querySelector("#lose-screen");
+// const winScreen = document.querySelector("#win-screen");
+// const loseScreen = document.querySelector("#lose-screen");
 
 function changeScreen(prevScreen, newScreen) {
     prevScreen.style.display = "none";
     newScreen.style.display = "flex";
 }
 
-
 /* ===============================
    2. Start Screen
 ================================= */
 
-const startBtn = document.querySelector(".btn.start");
+const startBtn = document.querySelector("#startBtn");
 
 startBtn.addEventListener('click', () => {
     changeScreen(startScreen, gameScreen);
 })
 
+/* ===============================
+   2. Game Screen
+================================= */
+
 let dialogueBox = document.querySelector(".dialogue-box");
+// TODO: ANIMATION TYPING TEXT
 dialogueBox.textContent = `A wild opponent appears. Prepare for battle! Rock, Paper, or Scissors?`;
 
 /* ===============================
@@ -35,18 +39,7 @@ dialogueBox.textContent = `A wild opponent appears. Prepare for battle! Rock, Pa
 let humanScore = 0;
 let computerScore = 0;
 
-const choices = document.querySelector(".choices");
-const results = document.querySelector(".results");
-
-const roundResults = document.createElement("p");
-const humanScoreCount = document.createElement("p");
-const computerScoreCount = document.createElement("p");
-const gameResults = document.createElement("p");
-
-results.appendChild(roundResults);
-results.appendChild(humanScoreCount);
-results.appendChild(computerScoreCount);
-results.appendChild(gameResults);
+const choices = document.querySelector(".choice-box");
 
 /* ===============================
    4. Game Logic Functions
@@ -75,15 +68,16 @@ function playRound(humanChoice, computerChoice){
     console.log(`Player: ${humanChoice}`)
     console.log(`Computer: ${computerChoice}`)
     
+    dialogueBox.textContent = ""; // TEMP
     if (humanChoice === computerChoice) {
-        roundResults.textContent = `It's a tie! Both chose ${humanChoice}. No score.`;
+        dialogueBox.textContent = `It's a tie! Both chose ${humanChoice}. No score.`;
         return;
     } else if (winsAgainst[humanChoice] === computerChoice) {
         humanScore++;
-        roundResults.textContent = `You win this round! ${humanChoice} beats ${computerChoice}`;
+        dialogueBox.textContent = `You win this round! ${humanChoice} beats ${computerChoice}`;
     } else {
         computerScore++;
-        roundResults.textContent = `You lose this round! ${computerChoice} beats ${humanChoice}`;
+        dialogueBox.textContent = `You lose this round! ${computerChoice} beats ${humanChoice}`;
     }
 }
 
@@ -111,13 +105,15 @@ choices.addEventListener('click', (event) => {
     const computerChoice = getComputerChoice();    
     playRound(humanChoice, computerChoice);
     
+    console.log(`Player Score: ${humanScore}`)
+    console.log(`Computer Score: ${computerScore}`)
     // Display the running score
-    humanScoreCount.textContent = `Player Score: ${humanScore}`;
-    computerScoreCount.textContent = `Computer Score: ${computerScore}`;
-    
+    // humanScoreCount.textContent = `Player Score: ${humanScore}`;
+    // computerScoreCount.textContent = `Computer Score: ${computerScore}`;
+
     // Announce winner of the game
-    if (humanScore === 5 || computerScore === 5) {
-        gameResults.textContent = humanScore > computerScore ? 'You won the game!' : 'You lose! Better luck next time.';
-    }
+    // if (humanScore === 5 || computerScore === 5) {
+    //     gameResults.textContent = humanScore > computerScore ? 'You won the game!' : 'You lose! Better luck next time.';
+    // }
 });
 
